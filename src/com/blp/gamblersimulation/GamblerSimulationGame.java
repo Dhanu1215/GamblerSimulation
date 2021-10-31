ccddea8474433f51596ff043ed1d8b8b1a71aafe
Asus <dhanashreehakke7@gmail.com>
2021-10-31 14:25:30 +0530

    Added: Welcome message is added

---
+++ src/com/blp/gamblersimulation/GamblerSimulationGame.java
@@ -0,0 +1,11 @@
+package com.blp.gamblersimulation;
+
+/**
+ * Gambler Simulation Game
+ */
+public class GamblerSimulationGame {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler Simulation Game Project");
+    }
+}
